eaf37d47526f096c4b5e9d30d76fb07467af1656
DRypa <dr.drypa.k@mail.ru>
2015-11-18 09:58:23 +0300

    repository pattern example added

---
+++ Other/Repository/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Repository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUserRepository users = new UserRepository();
+            users.Save(new User{Id = 0,Name = "first"});
+            users.Save(new User{Id = 1,Name = "second"});
+            var secondUser = users.Find(1);
+            Console.WriteLine(secondUser.Name);
+            var allUsers = users.GetAll();
+            foreach (User user in allUsers)
+            {
+                Console.WriteLine(user.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Other/Repository/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Repository
+{
+    [Serializable]
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
